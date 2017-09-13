d8b8e0885e8117e708f525211babd82c315b3bad
Frank Hamand <frankhamand@gmail.com>
2017-09-13 16:39:55 +0100

    0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 requirements = [str(req.req) for req in parse_requirements('requirements.txt', session=False)]
 
 setup(name='cabot_check_cloudwatch',
-      version='0.1.0',
+      version='0.1.1',
       description='A clouwatch check plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='techteam@arachnys.com',
